76d69e1af945bbd81be7a0158b3d486db1fa7a0b
Feiyang Xie <feiyang3cat@outlook.com>
2026-04-16 15:35:22 -0700

    made internal functions of ms private

--- service/history/interfaces/mutable_state.go
+++ service/history/interfaces/mutable_state.go
@@ -402,7 +402,6 @@ type (
 		// time-skipping related methods
 		AddWorkflowExecutionTimeSkippingTransitionedEvent(ctx context.Context) (*historypb.HistoryEvent, error)
 		ApplyWorkflowExecutionTimeSkippingTransitionedEvent(ctx context.Context, event *historypb.HistoryEvent) error
-		ShouldExecuteTimeSkipping() bool
 		GetTimeSkippingVirtualTime() time.Time
 	}
 )
